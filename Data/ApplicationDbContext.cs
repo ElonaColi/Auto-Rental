7085f3f71c339ee3bae0ccf0a62e598a7f99b523
ElonaColi <elonnaacoli@gmail.com>
2025-11-21 20:09:59 +0100

    Initial project setup: Cars module, ApplicationDbContext, and added .gitignore

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Auto_Rental.Models;
+namespace Auto_Rental.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Car> Cars { get; set; }
+    }
+}
