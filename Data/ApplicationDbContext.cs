fe02c90aa310a5553af2d5b793bddee52b447e29
Agnesa Ademaj <agnesaademaj@gmail.com>
2025-12-02 15:55:25 +0100

    Create Customer Model, DbSet, and Migration

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,14 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Auto_Rental.Models;
+
 namespace Auto_Rental.Data
 {
-    public class ApplicationDbContext : DbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-        }
-        public DbSet<Car> Cars { get; set; }
-        public DbSet<Rental> Rentals { get; set; }
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
 
-    }
+		public DbSet<Customer> Customers { get; set; }
+		public DbSet<Car> Cars { get; set; }
+		public DbSet<Rental> Rentals { get; set; }
+	}
 }
